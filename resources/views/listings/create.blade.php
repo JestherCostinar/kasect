2cdd8cc2195d5d688c557072a9374816af4e08f7
Jesther Costinar <jesther.jc15@gmail.com>
2022-11-03 05:41:33 +0800

    Keep user input stay in form

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -22,6 +22,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="company"
+                                value="{{ old('company') }}"
                             />
                             @error('company')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -37,6 +38,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="title"
                                 placeholder="Example: Senior Laravel Developer"
+                                value="{{ old('title') }} "
                             />
                             @error('title')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -54,7 +56,8 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="location"
                                 placeholder="Example: Remote, Boston MA, etc"
-                            />
+                                value="{{ old('location') }} "
+                                />
                             @error('location')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
@@ -68,7 +71,8 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="email"
-                            />
+                                value="{{ old('email') }} "
+                                />
                             @error('email')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
@@ -85,7 +89,8 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="website"
-                            />
+                                value="{{ old('website') }} "
+                                />
                             @error('website')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
@@ -100,7 +105,8 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="tags"
                                 placeholder="Example: Laravel, Backend, Postgres, etc"
-                            />
+                                value="{{ old('tags') }} "
+                                />
                             @error('tags')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
@@ -129,7 +135,7 @@
                                 name="description"
                                 rows="10"
                                 placeholder="Include tasks, requirements, salary, etc"
-                            ></textarea>
+                            >{{ old('description') }}</textarea>
                             @error('description')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
