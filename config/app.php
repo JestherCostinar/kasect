f8eee77f8c0888641fdc3ad331b18b29a7729c5a
Jesther Costinar <jesther.jc15@gmail.com>
2022-11-03 02:34:08 +0800

    Include barryvdh debugbar for debugging of codes

--- config/app.php
+++ config/app.php
@@ -159,6 +159,8 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+
+        Barryvdh\Debugbar\ServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
